154b79af1f9b33fe10209dd1ad34a7fd7c135106
huangk <huangk@minstone.com.cn>
2021-01-03 18:22:00 +0800

    no message

---
+++ rsocket-case-01/src/main/java/com/gotkx/rsocketcase01/RsocketCase01Application.java
@@ -0,0 +1,13 @@
+package com.gotkx.rsocketcase01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketCase01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketCase01Application.class, args);
+    }
+
+}
---
+++ rsocket-case-01/src/test/java/com/gotkx/rsocketcase01/RsocketCase01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotkx.rsocketcase01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketCase01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
